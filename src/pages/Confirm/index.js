1acdf8db559cb5ddb6e2e46254701b88a20cdf40
DanielThemoteo <daniel.themoteo@gmail.com>
2021-05-21 20:31:38 -0300

    setup react

---
+++ src/pages/Confirm/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Index(){
+    return(
+        <h1>
+            Confirm page
+        </h1>
+    );
+}
+
+export default Index;
---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {BrowserRouter,Route, Switch, Redirect } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Confirm from './pages/Confirm';
+import Shop from './pages/Shop';
+
+function Routes() {
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/confirm' component={Confirm} />
+                <Route path='/shop' component={Shop} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
