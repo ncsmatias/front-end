a1d581a38fb4c78ba0bcaaaa94c5f2282fcb8b48
Felipe Canuto <felipee.canuto@hotmail.com>
2021-06-01 19:54:54 -0300

    modal com form de endereco de entrega

--- src/pages/Shop/index.js
+++ src/pages/Shop/index.js
@@ -1,22 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './styles.css';
-
+import { Modal, Button, Form } from 'react-bootstrap'
 import Header from './../../components/Header';
 import Footer from './../../components/Footer';
 
 function Index() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <>
             <Header />
             <div className="page">
                 <div className="main-content">
                     <div className="content-form">
-                        <h6>Nome</h6>
+                        <div className="user">
+                            <h6>Nome</h6>
                             Daniel
                             <h6>Email</h6>
                             ae@ae.br
                         </div>
+
+                        <div className="endereco">
+                            <Button variant="primary" onClick={handleShow}>
+                                Endereço de Entrega
+                            </Button>
+
+                            <Modal show={show} onHide={handleClose}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>Endereço de Entrega</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>
+                                    <Form>
+                                        <Form.Group className="mb-3" controlId="formBasic">
+                                            <Form.Label>Endereço</Form.Label>
+                                            <Form.Control type="text" placeholder="Endereço" />
+                                        </Form.Group>
+
+                                        <Form.Group className="mb-3" controlId="formBasic">
+                                            <Form.Label>Bairro</Form.Label>
+                                            <Form.Control type="text" placeholder="Bairro" />
+                                        </Form.Group>
+
+                                        <Form.Group className="mb-3" controlId="formBasic">
+                                            <Form.Label>Número</Form.Label>
+                                            <Form.Control type="number" placeholder="Número" />
+                                        </Form.Group>
+
+                                        <Form.Group className="mb-3" controlId="formBasic">
+                                            <Form.Label>Complemento</Form.Label>
+                                            <Form.Control type="text" placeholder="Complemento" />
+                                        </Form.Group>
+
+                                        <Form.Group className="mb-3" controlId="formBasic">
+                                            <Form.Label>Estado</Form.Label>
+                                            <Form.Control type="text" placeholder="Estado" />
+                                        </Form.Group>
+
+                                        <Form.Group className="mb-3" controlId="formBasic">
+                                            <Form.Label>Cidade</Form.Label>
+                                            <Form.Control type="text" placeholder="Cidade" />
+                                        </Form.Group>
+                                    </Form>
+                                </Modal.Body>
+                                <Modal.Footer>
+                                    <Button variant="secondary" onClick={handleClose}>
+                                        Cancelar
+                                    </Button>
+                                    <Button variant="primary" onClick={handleClose}>
+                                        Salvar endereço
+                                    </Button>
+                                </Modal.Footer>
+                            </Modal>
+                        </div>
+                    </div>
                     <div className="content-shop">
                         <div className="product">
                             <div className="title">Meu carrinho</div>
