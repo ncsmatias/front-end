7f9d78ad986a78d2d12f3af056967e5a97187461
Evandro Duarte <duarte.evandro@gmail.com>
2021-06-04 11:54:44 -0300

    colocando mocks, faltando deseleção de botoes de categoria

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,86 +10,86 @@ import { Card, ToggleButton, ButtonGroup, Button } from "react-bootstrap";
 
 const cardInfo = [
   {
-    image: "/assets/cadeira_teste.jpg",
-    title: "Camiseta 1",
-    text: "100,00",
+    image: "/assets/camiseta_1.jpg",
+    title: "Camisa Palmeiras I 21/22 Torcedor Puma Masculina",
+    text: "250,00",
     id: "1",
     categoria: "camiseta",
   },
   {
-    image: "/assets/cadeira_teste.jpg",
-    title: "Tenis 1",
+    image: "/assets/tenis_1.jpg",
+    title: "Tênis Nike Lebron Witness V",
     text: "200,00",
     id: "2",
     categoria: "tenis",
   },
   {
-    image: "/assets/cadeira_teste.jpg",
-    title: "Camiseta 2",
+    image: "/assets/camiseta_2.jpg",
+    title: "Camisa Palmeiras I 21/22 Torcedor Puma Masculina",
     text: "300,00",
     id: "3",
     categoria: "camiseta",
   },
   {
-    image: "/assets/cadeira_teste.jpg",
-    title: "Blusa 1",
+    image: "/assets/blusa_1.jpg",
+    title: "Blusa Palmeiras Puma Manga Longa Branca",
     text: "400,00",
     id: "4",
     categoria: "blusa",
   },
   {
-    image: "/assets/cadeira_teste.jpg",
-    title: "Acessorio 1",
+    image: "/assets/acessorio_1.jpg",
+    title: "Mochila Palmeiras SEP BP 2017/2018",
     text: "500,00",
     id: "5",
     categoria: "acessorio",
   },
   {
-    image: "/assets/cadeira_teste.jpg",
-    title: "Blusa 2",
-    text: "100,00",
+    image: "/assets/blusa_2.jpg",
+    title: "Blusa Palmeiras Puma Manga Longa Verde",
+    text: "410,00",
     id: "6",
     categoria: "blusa",
   },
   {
-    image: "/assets/cadeira_teste.jpg",
-    title: "Blusa 3",
+    image: "/assets/blusa_3.jpg",
+    title: "Blusa Moletom Treino Palmeiras Branca Adidas",
     text: "200,00",
     id: "7",
     categoria: "blusa",
   },
   {
-    image: "/assets/cadeira_teste.jpg",
-    title: "Camiseta 3",
+    image: "/assets/camiseta_3.jpg",
+    title: "Camisa Palmeiras Palestra Pattern Masculina",
     text: "300,00",
     id: "8",
     categoria: "camiseta",
   },
   {
-    image: "/assets/cadeira_teste.jpg",
-    title: "Acessorio 2",
+    image: "/assets/acessorio_2.jpg",
+    title: "Mala Puma Palmeiras Liga Medium",
     text: "400,00",
     id: "9",
     categoria: "acessorio",
   },
   {
-    image: "/assets/cadeira_teste.jpg",
-    title: "Camiseta 4",
+    image: "/assets/camiseta_4.jpg",
+    title: "Camisa Palmeiras 1996 Retrô - Verde",
     text: "500,00",
     id: "10",
     categoria: "camiseta",
   },
   {
-    image: "/assets/cadeira_teste.jpg",
-    title: "Bermuda 1",
-    text: "500,00",
+    image: "/assets/bermuda_1.jpg",
+    title: "Bermuda Palmeiras Adidas Treino Masculina",
+    text: "80,00",
     id: "11",
     categoria: "bermuda",
   },
   {
-    image: "/assets/cadeira_teste.jpg",
-    title: "Bermuda 2",
-    text: "500,00",
+    image: "/assets/bermuda_2.jpg",
+    title: "Bermuda Palmeiras Microfibra Masculina",
+    text: "80,00",
     id: "12",
     categoria: "bermuda",
   },
@@ -97,6 +97,41 @@ const cardInfo = [
 
 function Index() {
   const [botaoSelecionado, SetBotaoSelecionado] = useState(0);
+  const [carrinho, setProdutoCarrinho] = useState([]);
+  const [index, setIndex] = useState(0);
+
+  const rednerCard = (card, index) => {
+    return (
+      <Card
+        id={card.id}
+        style={{
+          width: "18rem",
+          marginLeft: "10px",
+          marginTop: "15px",
+          height: "450px",
+        }}
+        key={index}
+      >
+        <Card.Img
+          id={"imagem-" + card.id}
+          variant="top"
+          src={process.env.PUBLIC_URL + card.image}
+          alt={card.categoria}
+        />
+        <Card.Body>
+          <Card.Title id={"produto-" + card.id}>{card.title}</Card.Title>
+          <Card.Text id={"valor-" + card.id}>R$ {card.text}</Card.Text>
+          <Button
+            style={{ marginLeft: "2rem" }}
+            variant="light"
+            onClick={() => pegarProduto(card.id)}
+          >
+            Adicionar ao carrinho
+          </Button>
+        </Card.Body>
+      </Card>
+    );
+  };
 
   function filtrar(categoria, botao) {
     if (botao === botaoSelecionado) {
@@ -110,7 +145,7 @@ function Index() {
         let div = document.getElementById(j);
         div.style.display = "block";
       }
-  
+
       for (let i = 1; i <= cardInfo.length; i++) {
         if (document.querySelector("#imagem-" + i).alt !== categoria) {
           let div = document.getElementById(i);
@@ -119,6 +154,25 @@ function Index() {
       }
       SetBotaoSelecionado(botao);
     }
+  }
+
+  function pegarProduto(produto_id) {
+    // console.log(document.querySelector("#imagem-" + produto_id).src);
+    // console.log(document.getElementById("produto-" + produto_id).innerText);
+    // console.log(document.querySelector("#valor-" + produto_id).innerHTML);
+
+    const data = {
+      produto: document.getElementById("produto-" + produto_id).innerText,
+      valor: document.querySelector("#valor-" + produto_id).innerHTML,
+      imagem: document.querySelector("#imagem-" + produto_id).src,
+    };
+
+    //Salvando os dados para serem usados pelo
+    sessionStorage.setItem(index, JSON.stringify(data));
+
+    // console.log(JSON.parse(sessionStorage.getItem(index)));
+
+    setIndex(index + 1);
   }
 
   return (
@@ -157,45 +211,6 @@ function Index() {
       <Footer />
     </div>
   );
-}
-
-const rednerCard = (card, index) => {
-  return (
-    <Card
-      id={card.id}
-      style={{
-        width: "18rem",
-        marginLeft: "10px",
-        marginTop: "15px",
-        height: "450px",
-      }}
-      key={index}
-    >
-      <Card.Img
-        id={"imagem-" + card.id}
-        variant="top"
-        src={process.env.PUBLIC_URL + card.image}
-        alt={card.categoria}
-      />
-      <Card.Body>
-        <Card.Title id={"produto-" + card.id}>{card.title}</Card.Title>
-        <Card.Text id={"valor-" + card.id}>R$ {card.text}</Card.Text>
-        <Button
-          style={{ marginLeft: "2rem" }}
-          variant="light"
-          onClick={() => pegarProduto(card.id)}
-        >
-          Adicionar ao carrinho
-        </Button>
-      </Card.Body>
-    </Card>
-  );
-};
-
-function pegarProduto(produto_id) {
-  console.log(document.querySelector("#imagem-" + produto_id).src);
-  console.log(document.getElementById("produto-" + produto_id).innerText);
-  console.log(document.querySelector("#valor-" + produto_id).innerHTML);
 }
 
 export default Index;
