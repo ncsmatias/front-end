5a3626e573eef60925cb10ad11f3ceec3090988b
ncsmatias <nicolasmarcelo30@gmail.com>
2021-08-07 17:42:16 -0300

    fix: new database route

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.73:3333',
+  baseURL: 'http://localhost:3333',
 });
 axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
 export default api;
